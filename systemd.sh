ddc19dbcb290ec6e6708d788cb1407da33d0cd67
Mark Karpeles <magicaltux@gmail.com>
2021-07-22 21:49:20 +0900

    systemd: add support for setting $root

--- systemd.sh
+++ systemd.sh
@@ -2,6 +2,9 @@
 set -e
 
 PROJECT=$1
+if [ x"$ROOT" = x ]; then
+	ROOT=""
+fi
 
 if [ x"$PROJECT" = x ]; then
 	echo >&2 "Error: please specify project name. $0 [name]"
@@ -9,7 +12,7 @@ if [ x"$PROJECT" = x ]; then
 fi
 
 # use /usr/sbin
-cd /usr/sbin
+cd "$ROOT/usr/sbin"
 
 if [ -f "$PROJECT" ]; then
 	echo >&2 "Error: project already installed?"
@@ -22,7 +25,7 @@ curl -s https://raw.githubusercontent.com/KarpelesLab/make-go/master/get.sh | /b
 # let's install systemd script
 echo "Installing systemd script ${PROJECT}..."
 
-cat >"/lib/systemd/system/${PROJECT}.service" <<EOF
+cat >$ROOT"/lib/systemd/system/${PROJECT}.service" <<EOF
 [Unit]
 Description=${PROJECT} daemon
 After=network-online.target
@@ -42,7 +45,14 @@ $SVC_EXTRA
 WantedBy=multi-user.target
 EOF
 
-systemctl enable "$PROJECT"
-if [ x"$SKIP_START" = x ]; then
-	systemctl start "$PROJECT"
+if [ x"$ROOT" = x ]; then
+	systemctl enable "$PROJECT"
+	if [ x"$SKIP_START" = x ]; then
+		systemctl start "$PROJECT"
+	fi
+else
+	# we cannot use systemctl because root may not be the same architecture
+	# Created symlink /etc/systemd/system/multi-user.target.wants/daemonmaster.service → /lib/systemd/system/daemonmaster.service.
+	ln -s "/lib/systemd/system/${PROJECT}.service" "${ROOT}/etc/systemd/system/multi-user.target.wants/${PROJECT}.service"
+	echo "Created symlink ${ROOT}/etc/systemd/system/multi-user.target.wants/${PROJECT}.service → /lib/systemd/system/${PROJECT}.service"
 fi
